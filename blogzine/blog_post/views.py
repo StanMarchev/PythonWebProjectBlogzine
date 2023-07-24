19bb84192708cfb6744c2fe6e98706a6ddbc0232
StanMarchev <stanimir.marchev@outlook.com>
2023-07-24 18:32:28 +0100

    blog_post

--- blogzine/blog_post/views.py
+++ blogzine/blog_post/views.py
@@ -1,12 +1,23 @@
 from django import views
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from blogzine.blog_post.models import CreatePost
+
 
 # Create your views here.
 class PostDetailsView(views.View):
     pass
 
-class PostCreateView(views.View):
-    pass
+class PostCreateView(CreateView):
+    model = CreatePost
+    template_name = 'blog_post/dashboard-post-create.html'
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 class PostEditView(views.View):
     pass
--- blogzine/settings.py
+++ blogzine/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from django.urls import reverse_lazy
@@ -146,3 +146,9 @@ LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
 
 LOGOUT_REDIRECT_URL = reverse_lazy('home')
 LOGIN_URL = reverse_lazy('sign-in')
+
+
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
