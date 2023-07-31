4d3201bd50142300ccfd05f3a14f09f7d558a819
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2023-07-31 22:14:59 +0100

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blogzine.common.urls')),
+    path('accounts/', include('blogzine.blog_auth.urls')),
+    path('posts/', include('blogzine.blog_post.urls')),
+    path('shop/', include('blogzine.blog_shop.urls')),
+
+    ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
